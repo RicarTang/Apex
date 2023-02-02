6406259dbef8aaf6f73a0038e643bec1bbd6d94a
RicarTang <357965604@qq.com>
2023-02-02 10:32:46 +0800

    修改log_util模块的逻辑

--- src/utils/log_util.py
+++ src/utils/log_util.py
@@ -6,8 +6,12 @@ import config
 def set_log():
     logger = logging.getLogger(__name__)
     logger.setLevel(config.stream_log_level)
+    # 判断log文件夹是否存在，不存在创建log目录
+    is_exists = os.path.exists(os.path.join(os.path.dirname(__file__),'../log'))
+    if not is_exists:
+        os.makedirs(os.path.join(os.path.dirname(__file__),'../log'))
     # 文件日志处理器
-    file_handler = logging.FileHandler(os.path.join(os.path.dirname(__file__),'../log/log.log'), encoding='utf-8')
+    file_handler = logging.FileHandler(os.path.join(os.path.dirname(__file__), '../log/log.log'), encoding='utf-8')
     file_handler.setLevel(config.file_log_level)
     log_format = logging.Formatter(config.log_formatter)
     file_handler.setFormatter(log_format)
