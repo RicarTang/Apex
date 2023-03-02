f9d2a51943f1b05970f7dc58e3ea05710a65ed59
RicarTang <357965604@qq.com>
2023-03-02 11:25:09 +0800

    修改login schemas逻辑

--- src/schemas.py
+++ src/schemas.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 
 
 class BaseSchema(BaseModel):
+    """success状态"""
     success: bool = Field(default=True)
 
 
@@ -12,6 +13,7 @@ class Status(BaseSchema):
 
 
 class User(BaseModel):
+    """用户"""
     username: str
     surname: Optional[str]
     name: Optional[str]
@@ -19,38 +21,44 @@ class User(BaseModel):
 
 
 class UserIn(User):
+    """用户request schema"""
     password: str = Field(min_length=6, max_length=20)
 
 
 class UserOut(BaseSchema):
+    """单用户response schema"""
     data: User_Pydantic
 
-    class Config:
-        orm_mode = True
+    # class Config:
+    #     orm_mode = True
 
 
 class UsersOut(BaseSchema):
+    """用户集response schema"""
     data: List[User_Pydantic]
 
-
-class Login(BaseSchema, extra=Extra.ignore):
+class UserPy(User_Pydantic,extra=Extra.ignore):
     """
     extra=Extra.ignore,表示忽略多的属性，
-    不加时，多了模型没有的属性会报错
+    不加时，多了模型没有的属性会报错，
+    这里忽略password属性
     """
-    data: Login_pydantic
-    # data: User_Pydantic
+    pass
+
+class Login(BaseSchema):
+    """登录response schema"""
+    data: UserPy
     access_token: str
     token_type: str
 
 
 class LoginIn(BaseModel):
+    """登录request schema"""
     username: str
     password: str
 
 
 # class LoginOut(BaseSchema):
-    
 
 
 class CommentIn(BaseModel):
