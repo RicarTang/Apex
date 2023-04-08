f8d2de92b077493837adb1c0406a73c03629e1c4
Tang <357965604@qq.com>
2023-04-08 16:08:45 +0800

    --dev新增black依赖

--- src/utils/background_task_util.py
+++ src/utils/background_task_util.py
@@ -2,6 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
-@scheduler.scheduled_job("interval",id="interval_test",seconds=3)
+
+# @scheduler.scheduled_job("interval", id="interval_test", seconds=3)
 def tast_test():
     print("hello")
