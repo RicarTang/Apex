f39db54d9822ba29f0de5a2f057e5777ca1f867a
Tang <357965604@qq.com>
2023-06-17 18:49:53 +0800

    修改role.py为admin.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.add_middleware(
 register_tortoise(
     app,
     config=TORTOISE_ORM,
-    generate_schemas=False,  # 重启服务时自动生成数据库表
+    generate_schemas=False,  # 重启服务时自动生成数据库表；关闭，改为使用aerich
     add_exception_handlers=True,
 )
 # router
--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,4 +1,4 @@
 from .users import user_api
 from .comments import comment_api
 from .test import test_api
-from .role import role_api
+from .admin import role_api
