aef94f638200f10ec849bf99056511196c3672be
Tang <357965604@qq.com>
2023-06-18 09:41:34 +0800

    修改models TimestampMixin字段modify_at为update_at

--- src/db/models.py
+++ src/db/models.py
@@ -7,7 +7,7 @@ class TimeStampMixin:
     """创建/更新时间"""
 
     created_at = fields.DatetimeField(auto_now_add=True, description="创建时间")
-    modified_at = fields.DatetimeField(auto_now=True, description="更新时间")
+    update_at = fields.DatetimeField(auto_now=True, description="更新时间")
 
 
 class Disabled(IntEnum):
