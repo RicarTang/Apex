ebb346cbfd3f982d48f8eb8ae737f7f3a4f160a8
tang <357965604@qq.com>
2023-07-17 22:44:29 +0800

    gunicorn使用配置文件启动,log日志标准输出

---
+++ gunicorn.conf.py
@@ -0,0 +1,37 @@
+#import multiprocessing
+
+#预加载资源
+preload_app = True
+
+# 并行工作进程数
+#workers = 5
+
+# 指定每个工作者的线程数
+threads = 4
+
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# 绑定端口
+bind = '0.0.0.0:4000'
+
+# 设置守护进程,将进程交给supervisor管理
+daemon = 'false'
+
+
+
+
+# 设置最大并发量
+worker_connections = 2000
+
+# 设置进程文件目录
+#pidfile = '/var/run/gunicorn.pid'
+
+# 日志标准输出
+accesslog = "-"
+errorlog = "-"
+loglevel = "debug"
+
+# 设置日志记录水平 
+loglevel = 'debug'
+
+
