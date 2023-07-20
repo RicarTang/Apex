b1e8c1f90a7af8336257548538464ad09fd3c93a
RicarTang <357965604@qq.com>
2023-07-20 15:31:07 +0800

    同步pro分支

--- config.py
+++ config.py
@@ -17,12 +17,13 @@ class BaseConfig(BaseSettings):
     # models
     MODELS_PATH = "src.db.models"
     # jwt相关
-    SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    SECRET_KEY: str
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
     # casbin
     RBAC_MODEL_PATH = os.path.join(
         ROOT_PATH, "src", "utils", "casbin", "rbac_model.conf"
     )
+
 
 config = BaseConfig()
