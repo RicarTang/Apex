b9047e8b3f325de25a5be6a8cff5c12dd8c0c4f4
RicarTang <357965604@qq.com>
2023-08-21 14:56:21 +0800

    迁移exception到core目录

---
+++ src/core/exception.py
@@ -0,0 +1,37 @@
+from fastapi import Request, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+
+
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    """自定义的HTTPException
+
+    Args:
+        request (Request): _description_
+        exc (HTTPException): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"code": exc.status_code, "message": f"{exc.detail}"},
+    )
+
+
+async def custom_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+):
+    """修改默认的请求验证错误模型
+
+    Args:
+        request (Request): _description_
+        exc (RequestValidationError): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"code": status.HTTP_400_BAD_REQUEST, "message": exc.errors()},
+    )
