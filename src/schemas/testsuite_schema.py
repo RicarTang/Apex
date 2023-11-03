861cc1996aaa53db1e1479e162b3f3f4a4cd4da0
RicarTang <357965604@qq.com>
2023-11-03 18:34:49 +0800

    测试套件内嵌测试用例列表；

--- src/schemas/testsuite_schema.py
+++ src/schemas/testsuite_schema.py
@@ -10,12 +10,13 @@ class TestSuiteIn(BaseModel):
     suite_no: str = Field(max_length=10, description="套件编号")
     suite_title: str = Field(max_length=50, description="套件名称/标题")
     remark: Optional[str] = Field(description="备注")
-    testcase_id: int = Field(gt=0, description="测试用例id")
+    testcase_id: Optional[int] = Field(gt=0, description="测试用例id")
 
 
 class TestSuiteTo(Testsuite_Pydantic):
     """测试套件response schema"""
 
+    # testcases: List[dict] = Field(description="套件包含的用例")
     pass
 
 
