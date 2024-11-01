dfc59d4dc8582b685efaaaf0cbdec2fe2ed0b403
Ricardo <107909844+RicarTang@users.noreply.github.com>
2024-11-01 16:42:08 +0800

    pydantic BaseConfig添加extra=allow通过任何环境变量

--- testframe_backend/config.py
+++ testframe_backend/config.py
@@ -9,7 +9,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class BaseConfig(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=Path(__file__).parent.parent / ".env", env_file_encoding="utf-8"
+        env_file=Path(__file__).parent.parent / ".env", env_file_encoding="utf-8",extra='allow'
     )
     # 目录相关
     # 根目录
