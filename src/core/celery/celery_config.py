1eb14b3668b17f03cde1e7916058d5ee964446cd
Tang <357965604@qq.com>
2025-07-12 09:59:45 +0800

    ..

--- src/core/celery/celery_config.py
+++ src/core/celery/celery_config.py
@@ -1,4 +1,5 @@
 # 导入task模块
+from celery.beat import crontab
 imports = ("src.core.celery.task.testcase_task", "src.core.celery.task.scheduled_task","src.core.celery.scheduler")
 # 使用utc时区
 enable_utc = False
@@ -7,11 +8,11 @@ timezone = "Asia/Shanghai"
 # started状态显示
 task_track_started = True
 # 定时任务配置
-# beat_schedule = {
-#     'statistics-dashbord-data': {
-#         'task': 'src.core.celery.task.scheduled_task.statistics_dashbord_data',  # task指向task name
-#         'schedule': crontab(hour="*/1",minute=0),  # 每个小时执行
-#     }
-# }
+beat_schedule = {
+    'statistics-dashbord-data': {
+        'task': 'src.core.celery.task.scheduled_task.statistics_dashbord_data',  # task指向task name
+        'schedule': crontab(minute="*"),
+    }
+}
 # 自定义调度器
 # beat_scheduler = "src.core.celery.scheduler.DatabaseScheduler"
